0c2785273a7c06521dac26c07e2b30ddaa1797cf
AndyWu1999 <wu.tianqi@outlook.com>
2020-06-26 16:51:14 -0500

    Tweaks

--- __init__.py
+++ __init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from route import index, login
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
-    app.add_url_rule('/', 'index', index)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:social_network.db"
+    db.init_app(app)
+    migrate.init_app(app,db)
+    app.add_url_rule('/index', 'index', index)
     app.add_url_rule('/login','login', login, methods=['GET','POST'])
     return app
 
---
+++ models.py
@@ -0,0 +1,12 @@
+from __init__ import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return 'id={}, username={}, email={}, password_hash={}'.format(
+            self.id, self.username, self.email, self.password_hash
+        )
