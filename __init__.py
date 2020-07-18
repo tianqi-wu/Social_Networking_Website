94f64fd28a4f321c0d3233a8081437630b782970
root <wu.tianqi@outlook.com>
2020-07-19 00:28:52 +0800

    Tweak

--- __init__.py
+++ __init__.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
 
+from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
-login_manager = LoginManager()
+#login_manager = LoginManager()
+
+#from route import index, login
 
 from route import index, login
 
 def create_app():
     app = Flask(__name__)
-    #app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:social_network.db"
-    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app,db)
     app.add_url_rule('/index', 'index', index)
     app.add_url_rule('/', 'index', index)
     app.add_url_rule('/login','login', login, methods=['GET','POST'])
--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 config_path = os.path.abspath(os.path.dirname(__file__))
 
 
+#    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///social_network.db"
+#    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(config_path, 'social_network.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
