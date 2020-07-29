b5b91f7776e5bb7ee18ee8102b69346aa45868e3
AndyWu1999 <wu.tianqi@outlook.com>
2020-07-29 17:28:28 +0800

    Test

--- route.py
+++ route.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, current_user
 from forms import LoginForm
 from models import User, Tweet
 
@@ -21,6 +21,8 @@ def index():
 
 
 def login():
+    if current_user.is_authenticated():
+        return redirect(url_for(index))
     form = LoginForm()
     if form.validate_on_submit():
         u = User.query.filter_by(username=form.username.data).first()
